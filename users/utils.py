6351fd34b329ef551ac3f1dd6eb6c4d5d89a4fca
saisrijabogasundram <saisrijabogasundram10@gmail.com>
2026-04-04 12:47:11 +0530

    debug: add Fast2SMS response logging

--- users/utils.py
+++ users/utils.py
@@ -2,10 +2,9 @@ import random
 import requests
 from django.core.cache import cache
 from django.conf import settings
-
 def send_sms_otp(phone_number):
     otp = str(random.randint(100000, 999999))
-    cache.set(f'otp_{phone_number}', otp, timeout=300)  # 5 mins expiry
+    cache.set(f'otp_{phone_number}', otp, timeout=300)
 
     response = requests.post(
         'https://www.fast2sms.com/dev/bulkV2',
@@ -20,11 +19,15 @@ def send_sms_otp(phone_number):
         }
     )
 
+    print("Fast2SMS status:", response.status_code)  # ← add this
+    print("Fast2SMS response:", response.text)        # ← add this
+
     data = response.json()
     if not data.get('return'):
         raise Exception(data.get('message', 'Failed to send OTP'))
 
     return otp
+
 
 def verify_sms_otp(phone_number, otp):
     cached_otp = cache.get(f'otp_{phone_number}')
